bef551ca822f196627a9539b6b16d3545e4d36d4
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-03-10 10:33:10 +0100

    main.py: VT100 : Creation d'une fonction vt100_clear_screen() VT100 : Creation d'une fonction vt100_move() Test des 2 fonctions : ok

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ def uart_init(num_uart):
 
     return uart_port
 
+def vt100_clear_screen(uart):  
+    uart.write(b"\x1b[2J\x1b[?25l")
+
+def vt100_move(uart, x, y):
+    uart.write("\x1b[{};{}H".format(y, x))
+
 # Initialisation de la liaison série 
 uart = uart_init(2)
 
 while(1):
+    vt100_move(uart, 5, 10)
     uart.write(b"0x00")
     pyb.delay(1000)
+    vt100_clear_screen(uart)
